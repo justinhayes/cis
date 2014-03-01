bc510c16b35da47dfda1ac306e7ba63ac069d420
Justin Hayes <jhayes@cloudera.com>
2014-02-28 16:32:05 -0800

    added initial code to make oryx rest call and interpret result

--- src/main/java/com/cloudera/solutions/cis/flume/OryxDistanceInterceptor.java
+++ src/main/java/com/cloudera/solutions/cis/flume/OryxDistanceInterceptor.java
@@ -2,7 +2,6 @@ package com.cloudera.solutions.cis.flume;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map.Entry;
 
 import org.apache.flume.Context;
 import org.apache.flume.Event;
@@ -11,6 +10,10 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.google.common.base.Preconditions;
+import com.sun.jersey.api.client.Client;
+import com.sun.jersey.api.client.ClientResponse;
+import com.sun.jersey.api.client.WebResource;
+import com.sun.jersey.api.client.config.DefaultClientConfig;
 
 public class OryxDistanceInterceptor implements Interceptor {
 
@@ -19,6 +22,7 @@ public class OryxDistanceInterceptor implements Interceptor {
 
 	private final Context context;
 	private final String oryxDistanceResource;
+	private Client restClient = null;
 
 	private static final String ORYX_SERVER_PROP_NAME = "oryxServer";
 	private static final String ORYX_DISTANCE_RESOURCE = "distanceToNearest";
@@ -26,19 +30,23 @@ public class OryxDistanceInterceptor implements Interceptor {
 	protected OryxDistanceInterceptor(Context context) {
 		Preconditions.checkNotNull(context);
 		this.context = context;
-		this.oryxDistanceResource = "http://" + context.getString(ORYX_SERVER_PROP_NAME) + "/" + ORYX_DISTANCE_RESOURCE;
-		logger.info("Using this resource to get distance: " + this.oryxDistanceResource);
+		this.oryxDistanceResource = "http://" + context.getString(ORYX_SERVER_PROP_NAME) + "/" + ORYX_DISTANCE_RESOURCE + "/";
+		logger.info("Using this resource to get distance: {}.", this.oryxDistanceResource);
+		
+		//TODO get threadsafe HTTP connection to server
+		//TODO figure out most efficient way to do REST calls
+		//TODO use a connection pool to increase throughput?
+		DefaultClientConfig restClientConfig = new DefaultClientConfig();
+		this.restClient = Client.create(restClientConfig);
 	}
 
 	@Override
 	public void close() {
-		// TODO Auto-generated method stub
 
 	}
 
 	@Override
 	public void initialize() {
-		// TODO Auto-generated method stub
 
 	}
 
@@ -46,19 +54,34 @@ public class OryxDistanceInterceptor implements Interceptor {
 	public Event intercept(Event event) {
 		String body = new String(event.getBody());
 		
-//		logger.info("Getting distance via " + this.oryxDistanceResource + "/" + body);
+		//make rest call to get distance
+		WebResource webResource = this.restClient.resource(this.oryxDistanceResource + body);
+		ClientResponse response = webResource.accept("application/json").get(ClientResponse.class);
+		if (response.getStatus() != 200) {
+			logger.error("Got return code {}.", response.getStatus());
+			throw new RuntimeException("Failed : HTTP error code : " + response.getStatus());
+		}
 		
-//		logger.info("  Headers:");
-//	    for (Entry<String, String> entry : event.getHeaders().entrySet()) {
-//	    	logger.info("    " + entry.getKey() + "=" + entry.getValue());
-//	    }
+		String output = response.getEntity(String.class);
+		Double distance = new Double(output);
 		
-		//TODO get HTTP connection to rest server, make rest call, interpret result, add flag/field if this is a positive
-	    boolean isEvent = false;
-	    
+		//interpret distance to decide if this is an outlier
+		boolean isEvent = isOutlier(distance);
+		
+		if (logger.isDebugEnabled()) {
+			logger.debug("isEvent={} for distance {}", isEvent, distance);
+		}
+		
 	    event.getHeaders().put("outlier", (isEvent ? "1" : "0"));
 		
 		return event;
+	}
+
+	//TODO implement
+	private boolean isOutlier(Double distance) {
+		boolean isOutlier = Math.round(distance) % 2 == 0;
+		
+		return isOutlier;
 	}
 
 	@Override
